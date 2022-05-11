302bef575773d2b526da93b6fd5e9e91c30e12bb
Amiran Malania <amalania@qarva.com>
2022-05-11 22:48:03 +0400

    Add ring solution

--- Week07/Cicle.cpp
+++ Week07/Cicle.cpp
@@ -1,11 +1,7 @@
-#include <iostream>
-
-class Circle {
-    private:
-        double radius; // proprety
+class circle {
+        double radius;
     
-    public:
-        Circle(){
+        Circle(double radius){
             radius = 1;
         }
 
@@ -13,31 +9,20 @@ class Circle {
             radius = newRadius;
         }
 
-        double getRadius(){
-            return radius;
-        }
-        
         double getArea(){
             return 3.14159 * radius * radius;
         }
-
-
 };
 
 int main(){
     Circle c1; // Default consturctor is called
-    std::cout << "Area of a cirlce with radius " << c1.getRadius() << " is " << c1.getArea() << std::endl; 
+    std::cout << "Area of a cirlce with radius " << c1.radius << " is " << c1.getArea() << std::endl; 
 
-    /*
     Circle c2(3);
-    //std::cout << "Area of a cirlce with radius " << c2.radius << " is " << c2.getArea() << std::endl; 
+    std::cout << "Area of a cirlce with radius " << c2.radius << " is " << c2.getArea() << std::endl; 
 
-    Circle c3(true, 3.5);
+    Circle c3(3.5);
     std::cout << "Area of a cirlce with radius " << c3.radius << " is " << c3.getArea() << " and its fileed" << std::endl;
-
-    c3.radius = 12.5;
-    std::cout << "Area of a cirlce with radius " << c3.radius << " is " << c3.getArea() << " and its fileed" << std::endl; 
-    */
 
     return 0;
 }
