6649939e1343e3c2afbf7b8efa1fcb042962fa74
Amiran Malania <amalania@qarva.com>
2022-06-06 18:57:37 +0400

    Almost done with Week 11

---
+++ Week11/wrongReverse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int* reverse(const int* list, int size){
+	// TODO : Reverse the array
+
+}
+
+void printArray(const int* list, int size){
+	// TODO : Print the array
+}
+
+int main(){
+	int list[] = {1, 2, 3, 4, 5, 6};
+	int* p = reverse(list, 6);
+	printArray(p, 6);
+
+	return 0;
+}
